501274757cbe8c02676f4c853420c4a54c4ac268
littleGnAl <tearjeaker@hotmail.com>
2022-07-08 20:41:15 +0800

    Cleanup

--- lib/src/render/agora_video_view.dart
+++ lib/src/render/agora_video_view.dart
@@ -1,6 +1,5 @@
 import 'package:agora_rtc_ng/src/impl/video_view_controller_impl.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import '../impl/agora_video_view_impl.dart';
 
 /// The AgoraVideoView Class for rendering local and remote video.
@@ -11,7 +10,6 @@ class AgoraVideoView extends StatefulWidget {
   /// Controls the type of video to render:
   /// If you want to render video of the RtcEngine, see VideoViewController .
   /// If you want to render video of the media player, see MediaPlayerController .
-  @internal
   final VideoViewControllerBase controller;
 
   @override
